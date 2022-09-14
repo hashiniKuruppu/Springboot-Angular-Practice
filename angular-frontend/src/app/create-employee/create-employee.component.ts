dbf353773f090b55fcad9098ba81acd764b204e0
hashiniKuruppu <hkuruppu@outlook.com>
2022-09-14 06:21:41 +0530

    save employee info and redirect to employee list

--- angular-frontend/src/app/create-employee/create-employee.component.ts
+++ angular-frontend/src/app/create-employee/create-employee.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
-@Component({
+@Component({ 
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  employee: Employee = new Employee();
+
+  constructor(private employeeService: EmployeeService, 
+    private router: Router) { }
+
 
   ngOnInit(): void {
+  }
+
+
+  saveEmployee() {
+    this.employeeService.createEmployee(this.employee).subscribe (data => {
+      console.log(data); //print data if success
+      this.gotoEmployeeList();
+    },
+    error => console.log(error)); //print error
+  }
+
+
+  //when we submit employee data successfully we have to be able to go to  employee list page
+  gotoEmployeeList() {
+    this.router.navigate(['/employees']);
+  }
+
+
+  onSubmit() {
+    console.log(this.employee);
+    this.saveEmployee(); 
   }
 
 }
