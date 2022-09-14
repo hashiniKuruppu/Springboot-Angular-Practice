ea4d59edbed3035e4e647fe72a723eae03aebc83
hashiniKuruppu <hkuruppu@outlook.com>
2022-09-14 07:09:52 +0530

    create getEmployeeById
    create getEmployeeById

--- springboot-backend/src/main/java/com/hnk/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/hnk/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.hnk.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hnk.springboot.exception.ResourceNotFoundException;
 import com.hnk.springboot.model.Employee;
 import com.hnk.springboot.repository.EmployeeRepository ;
 
@@ -42,5 +44,13 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	//get employee by id
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Employee with id " + id + "does  not exist"));
+		return ResponseEntity.ok(employee);
 	}
 }
