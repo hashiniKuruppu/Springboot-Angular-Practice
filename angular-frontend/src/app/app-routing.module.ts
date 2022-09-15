381b420758aeb72ec6af347fe3ff2398f36c8451
hashiniKuruppu <hkuruppu@outlook.com>
2022-09-15 08:02:42 +0530

    create update-employee component, configure routes for update-employee component. update button in employee-list page

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 const routes: Routes = [
+
   {path: 'employees', component: EmployeeListComponent},
   {path: 'create-employee', component: CreateEmployeeComponent},
-  {path: '', redirectTo: 'employees', pathMatch: 'full'} //this will redirect the 'https:localhost/4200' 
-                                                         //to 'https:localhost/4200/employees' 
+  //this will redirect the 'https:localhost/4200' to 'https:localhost/4200/employees' 
+  {path: '', redirectTo: 'employees', pathMatch: 'full'}, 
+  {path: 'update-employee/:id', component: UpdateEmployeeComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
