d2e2c78bba90b3444058fcb25823a8b5a2755cea
hashiniKuruppu <hkuruppu@outlook.com>
2022-09-15 09:29:44 +0530

    connect getEmployeeById to rest api to client

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -20,4 +20,9 @@ export class EmployeeService {
   createEmployee(employee: Employee): Observable<Object>{
     return this.httpClient.post(`${this.baseURL}`, employee);
   }
+
+  getEmployeeById(id: number): Observable<Employee> {
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+
 }
